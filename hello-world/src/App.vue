bc6a3219dc5aa17859c29b27bc5a1358720306b6
IamVasudev <vasu2558452@gmail.com>
2021-12-16 23:24:51 +0530

    Hello

---
+++ hello-world/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h1>hello {{ name }}</h1>
+    <div>{{ phone }}</div>
+    <div v-html="hl"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      name: "batman",
+      hl: "<b>123123</b>",
+      phone: "234234",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
